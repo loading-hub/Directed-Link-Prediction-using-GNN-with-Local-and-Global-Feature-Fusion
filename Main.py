53bec8afed11fc743278f6194631b9a288eb02d8
loading-hub <72974699+loading-hub@users.noreply.github.com>
2024-11-14 20:25:46 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -40,10 +40,10 @@ global nkcore
 nkcore=[]
 global node_tag
 
-dst='cora_ml'#cora_ml
+dst='cora_ml'
 tr=0.5
 warnings.filterwarnings('ignore')
-def train(model: Model, x, edge_index):#对比学习
+def train(model: Model, x, edge_index):
     model.train()
     optimizer.zero_grad()
 
@@ -69,13 +69,13 @@ def test(model: Model, dataset, x, edge_index, edge_weight, y, final=False):
     model.eval()
     z = model(x, edge_index, edge_weight)
     embeding=z.cpu().detach().numpy()
-    #print(torch.cat(((torch.from_numpy(embeding)),(node_tag)),axis=1))#auc: 0.82746,
-    np.save('c://embeding.npy', torch.cat(((torch.from_numpy(embeding)),(node_tag)),axis=1))#auc: 0.85443,
-    #np.save('d://embeding.npy', embeding)
-    label_classification(z, y, data)#z为embeddings，y是点标签
+   
+    np.save('c://embeding.npy', torch.cat(((torch.from_numpy(embeding)),(node_tag)),axis=1))
+    
+    label_classification(z, y, data)
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Link Prediction')
-    # general settings
+ 
     dn=dst+'edge'
     parser.add_argument('--data-name', default=dn, help='network name')
     parser.add_argument('--train-name', default=None, help='train name')
@@ -88,7 +88,7 @@ if __name__ == '__main__':
                         help='random seed (default: 1)')
     parser.add_argument('--test-ratio', type=float, default=tr,
                         help='ratio of test links')
-    # model settings
+  
     parser.add_argument('--hop', default=3, metavar='S',
                         help='enclosing subgraph hop number, \
                         options: 1, 2,..., "auto"')
@@ -120,11 +120,11 @@ if __name__ == '__main__':
         net = data['net']
 
         attributes = None
-        # check whether net is symmetric (for small nets only)
+        
         if False:
             net_ = net.toarray()
             assert(np.allclose(net_, net_.T, atol=1e-8))
-        #Sample train and test links
+       
         train_pos, train_neg, test_pos, test_neg = sample_neg(net, args.test_ratio, max_train_num=args.max_train_num)
     else:
         args.train_dir = os.path.join(args.file_dir, 'data/{}'.format(args.train_name))
@@ -136,30 +136,28 @@ if __name__ == '__main__':
 
 
         net = ssp.csc_matrix((np.ones(len(train_idx)), (train_idx[:, 0], train_idx[:, 1])), shape=(max_idx+1, max_idx+1))
-        #net[train_idx[:, 1], train_idx[:, 0]] = 1  # add symmetric edges这是补齐不对称的边，在有向网络里应删掉
-        net[np.arange(max_idx+1), np.arange(max_idx+1)] = 0  # remove self-loops
-        #Sample negative train and test links
+       
+        net[np.arange(max_idx+1), np.arange(max_idx+1)] = 0  
+       
         train_pos = (train_idx[:, 0], train_idx[:, 1])
         test_pos = (test_idx[:, 0], test_idx[:, 1])
-        #负采样
-        train_pos, train_neg, test_pos, test_neg = sample_neg(net, train_pos=train_pos, test_pos=test_pos, max_train_num=args.max_train_num)#采集正负样本
+        
+        train_pos, train_neg, test_pos, test_neg = sample_neg(net, train_pos=train_pos, test_pos=test_pos, max_train_num=args.max_train_num)
 
 
     '''Train and apply classifier'''
-    A = net.copy()  # the observed network
-    A[test_pos[0], test_pos[1]] = 0  # mask test links
-    A[test_pos[1], test_pos[0]] = 0  # mask test links
+    A = net.copy()  
+    A[test_pos[0], test_pos[1]] = 0  
+    A[test_pos[1], test_pos[0]] = 0  
     A.eliminate_zeros()
-    #根据正负样本提取2跳子图
-
-    #对A进行对比学习把A替换成对比学习的数据
+   
 
 
 
 
     parser1 = argparse.ArgumentParser()
 
-    parser1.add_argument('--dataset', type=str, default=dst)  # default='DBLP'
+    parser1.add_argument('--dataset', type=str, default=dst)  
     parser1.add_argument('--gpu_id', type=int, default=0)
     parser1.add_argument('--config', type=str, default='config_digcl.yaml')
     parser1.add_argument('--alpha', type=float, default=0.1)
@@ -218,22 +216,21 @@ if __name__ == '__main__':
 
 
     data.edge_index=torch.tensor([Dx,Dy])
-    # 把data.edge_index替换成A+1的结构
-    # data = A
-
+   
+   
     G = nx.Graph()
     G = G.to_directed()
 
-    # G.add_nodes_from()
+   
     G.add_nodes_from(range(len(dataset[0].x)))
 
     for i in range(len(data.edge_index[0])):
         G.add_edge(int(data.edge_index[0][i]), int(data.edge_index[1][i]))
 
 
-    # nx.draw_networkx(G)
+    
     prall = community_louvain.best_partition(G)
-    #prall = nx.pagerank(G, alpha=0.01)  # 自己换成各种标记方法
+   
 
     nodes = list(G.nodes())
     la = np.array([partition[node] for node in nodes])
@@ -275,7 +272,7 @@ if __name__ == '__main__':
             print('wrong curr type')
             exit()
 
-        loss = train(model, data.x, data.edge_index)  # x为点的特征，y为对应标签,edge_index为边标签
+        loss = train(model, data.x, data.edge_index)  
 
         now = t()
         print(f'(T) | Epoch={epoch:03d}, loss={loss:.4f}, '
@@ -284,7 +281,7 @@ if __name__ == '__main__':
 
     print("=== Final ===")
     fea1=test(model, dataset, data.x, edge_index_init,
-         edge_weight_init, data.y, final=True)  # datdset不参与计算,x为点的特征，y为对应标签
+         edge_weight_init, data.y, final=True)  
 
 
 
@@ -308,21 +305,15 @@ if __name__ == '__main__':
 
     import scipy.io
 
-    #fea = scipy.io.loadmat(r'C:\Users\think\Desktop\link prediction\FFD-main\FFD-main\FFD\Python\data\cora_mlfea.mat')['fea']  # 读取mat文件
+   
     fea1 = np.load('c://embeding.npy')
-    #fea1=np.load('d://emb.npy')
-    #fea1=np.zeros_like(fea1)
-
-    #fea=np.zeros_like(fea1)
+    
 
 
-    train_graphs, test_graphs, max_n_label = links2subgraphs(A, train_pos, train_neg, test_pos, test_neg, args.hop, args.max_nodes_per_hop,fea1)#特征在这里输入
-    print(('# train: %d, # test: %d' % (len(train_graphs), len(test_graphs))))
+    train_graphs, test_graphs, max_n_label = links2subgraphs(A, train_pos, train_neg, test_pos, test_neg, args.hop, args.max_nodes_per_hop,fea1)
+    
     dim = len(train_graphs[0].node_features[0])
-    #print('feat_dim')
-    #print(dim)  #2879*2=5758
-    #quit()
-    #转化为线图
+    
     train_lines = to_linegraphs(train_graphs, max_n_label)
     test_lines = to_linegraphs(test_graphs, max_n_label)
 
@@ -333,7 +324,7 @@ if __name__ == '__main__':
 
 
 
-    # Model configurations
+    
 
     cmd_args.latent_dim = [32, 32, 32, 1]
     cmd_args.hidden = 128
@@ -345,13 +336,13 @@ if __name__ == '__main__':
     cmd_args.learning_rate = 5e-3
     cmd_args.batch_size = 50
     cmd_args.printAUC = True
-    cmd_args.feat_dim = (max_n_label + 1 + dim)*2   #max_n_label + 1+5758           5928
+    cmd_args.feat_dim = (max_n_label + 1 + dim)*2   
     cmd_args.attr_dim = 0
-    #分批次
+   
     train_loader = DataLoader(train_lines, batch_size=cmd_args.batch_size, shuffle=True)
     test_loader = DataLoader(test_lines, batch_size=cmd_args.batch_size, shuffle=False)
 
-    #分类器设置
+    
     classifier = Net(cmd_args.feat_dim, cmd_args.hidden, cmd_args.latent_dim, cmd_args.dropout)
     if cmd_args.mode == 'gpu':
         classifier = classifier.to("cuda")
@@ -365,7 +356,7 @@ if __name__ == '__main__':
     best_acc = 0
     best_acc_auc = 0
     con=''
-    #开始训练
+   
     maxauc = 0
     maxap = 0
     for epoch in range(cmd_args.num_epochs):
@@ -399,7 +390,4 @@ if __name__ == '__main__':
             best_acc = test_loss[3]
             best_acc_auc = test_loss[2]
 
-    #f = open('C:\\Users\\think\\Desktop\\实验数据\\'+dst+str(tr)+' label.txt', mode='w+')
-    #f.write(con)
-    #f.close()
-    print('auc: %.5f,ap: %.5f'%(maxauc,maxap))
+   
